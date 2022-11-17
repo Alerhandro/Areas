0911cace659defaff6dd6b9d42fc59df418f82d4
Byo <alerhandrojp@gmail.com>
2022-11-17 00:28:06 -0400

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Areas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // autor:Alerhandro
+            //calculo de areas
+            //variaveis 
+
+            Console.WriteLine("olá vamos calcular a area de um triangulo");
+                        
+            Console.WriteLine("Qual o tamanho da base");
+            decimal Base = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("base; " + Base);
+
+            Console.WriteLine("Qual a altura");
+            decimal Altura = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("altura; " + Altura);
+
+            
+            decimal resultado = (Base * Altura) / 2;
+
+            Console.WriteLine("area do triangulo; " + resultado);
+
+            Console.WriteLine("agora um quadrado");
+
+            Console.WriteLine("Qual a largura ");
+            decimal largura = Convert.ToDecimal(Console.ReadLine());
+
+            
+
+            decimal Resultado = (largura * largura);
+
+            Console.WriteLine("area do quadrado; " + Resultado);
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
